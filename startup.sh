0c8c08cbacd46132383cb380ae946458ce5bd2bf
Walky Hernandez <143972979+walkiwa@users.noreply.github.com>
2025-07-31 12:54:34 -0600

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Navegar al directorio donde Azure despliega tu código
-cd /home/site/wwwroot
-
-# Ejecutar Gunicorn. 'app:app' significa que buscará una instancia 'app' en el archivo 'app.py'.
-# --timeout 600 es útil para conexiones de DB que puedan tomar más tiempo.
-gunicorn --bind=0.0.0.0 --timeout 600 app:app
